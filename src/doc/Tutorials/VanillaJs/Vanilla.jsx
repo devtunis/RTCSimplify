bb7e44c59226aeacf7397ed673bcebe438558490
Gaith O’Nahdi <nahdigyth@gmail.com>
2026-03-05 22:33:27 +0100

    Refine short explanations in WebRTC tutorials
    Updated short explanations for WebRTC tutorials to be more concise.

--- src/doc/Tutorials/VanillaJs/Vanilla.jsx
+++ src/doc/Tutorials/VanillaJs/Vanilla.jsx
@@ -56,12 +56,12 @@ const Vanilla = () => {
       <CardTutorials 
                   videoImage={"/tutorials/pictuers/handelRemoteDesc.png"} 
                   linkvideo={"https://res.cloudinary.com/dfmdgsiid/video/upload/v1772714168/ice_wk2mr2.mp4"} 
-                  shortExplantion={"Learn how to handle ICE candidates in WebRTC by adding them to the peer connection and ensuring connectivity."}
+                  shortExplantion={"Learn how to handle ICE candidates in WebRTC ."}
                   description={"Exchange ICE candidates to establish the peer-to-peer connection."} 
                   infoSteps={[
-                  "Listen for 'icecandidate' events from your RTCPeerConnection",
-                  "Send ICE candidates to the remote peer via signaling server",
-                  "Use handleRemoteIceCandidate(candidate) to add remote ICE candidates"
+                     "Listen for ICE events",
+                    "Send candidates via signaling",
+                    "Add remote ICE candidates"
                 ]} 
                 />
 
@@ -69,7 +69,7 @@ const Vanilla = () => {
           <CardTutorials 
           videoImage={"/tutorials/pictuers/esablishConnection.png"} 
           linkvideo={"https://res.cloudinary.com/dfmdgsiid/video/upload/v1772712275/establishConnection_si2nru.mp4"} 
-          shortExplantion={"Learn how to handle an incoming WebRTC answer from the offer by setting the remote description and generating an answer to establish a peer-to-peer connection."}
+          shortExplantion={"Learn how to handle an incoming WebRTC answer from the offer."}
           description={"Generate the response for an incoming WebRTC offer."} 
           infoSteps={[
             "Use esablishConnection(answer)",
